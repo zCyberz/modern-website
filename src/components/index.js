fdea775332459e8b24a044a005c16088a42f821b
zCyberz <minhdang13496@gmail.com>
2023-11-28 01:43:05 +0700

    init

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import Business from "./Business";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+import Stats from "./Stats";
+
+export {
+  Navbar,
+  Billing,
+  Business,
+  CardDeal,
+  Clients,
+  CTA,
+  Footer,
+  Testimonials,
+  Hero,
+  Stats,
+};
