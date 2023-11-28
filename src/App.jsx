dac16b3aa5874947af96b5b17925847667bf5ab4
zCyberz <minhdang13496@gmail.com>
2023-11-28 16:04:44 +0700

    hero

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,14 @@ import {Navbar , Hero  ,Stats,
   Footer} from './components';
 
 const App = () => (
-    <div className='bg-slate-500 w-full overflow-hidden'>
+    <div className='bg-primary w-full overflow-hidden'>
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
           <Navbar/>
         </div>
 
       </div>
-      <div className={`bg-white ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
           <Hero/>
         </div>
